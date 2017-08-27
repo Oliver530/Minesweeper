95618138588b0ceea33c754f3a22b2af1a7c495a
Oliver Gerhardt <oli.gerhardt@gmail.com>
2017-08-27 17:55:29 +0200

    Initial commit

---
+++ src/test/java/CellTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by olivergerhardt on 27.08.17.
+ */
+public class CellTest {
+
+    @Test
+    public void nothing() {
+        Assert.assertEquals(1, 1);
+    }
+}
