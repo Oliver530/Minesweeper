d0b31f2e6a872354240199d2b0414ee96066d99e
Oliver Gerhardt <oli.gerhardt@gmail.com>
2017-09-02 11:52:21 +0200

    Removed GameController

--- src/main/java/minesweeper4java/Minesweeper4Java.java
+++ src/main/java/minesweeper4java/Minesweeper4Java.java
@@ -1,12 +1,17 @@
 package minesweeper4java;
 
+import model.GameModel;
+import view.console.ConsoleView;
+
 /**
  * Created by olivergerhardt on 31.08.17.
  */
 class Minesweeper4Java {
 
     public static void main(String[] args) {
-        new GameController().start();
+        MinesweeperGameModel gameModel = new GameModel();
+        MinesweeperView view = new ConsoleView(gameModel);
+        view.play();
     }
 
 }
